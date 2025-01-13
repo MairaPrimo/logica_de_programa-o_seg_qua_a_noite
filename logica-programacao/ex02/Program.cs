f9fef7335526de9c0a5ed5e8641b87ee9042c82e
MAIRA PRIMO DE SOUZA <maira.psouza@senacsp.edu.br>
2025-01-13 20:11:20 -0300

    primeiro commit

---
+++ logica-programacao/ex02/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ex.02 - Escreva um program que pergunte o nome do usuario, armazene esse nome numa variavel e apresente um mensagem de boas - vindas exibindo o nome de usuario
+
+Console.WriteLine("Informe o seu nome!");
+string name = Console .ReadLine();
+
+Console.WriteLine("seja bem - vindo (a)" + name);
---
+++ logica-programacao/ex03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Escrever programa que solicite dois numeros inteiros para o usuario e exiba a soma dos numeros
+
+Console.WriteLine("Digite o primeiro numero");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro numero:");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o quarto numero:");
+int numero4 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("A soma é:" + (numero1 + numero2 + numero3 + numero4));
+
+Console.WriteLine("A soma é: + {numero1 + numero2 + numero3 + numero4} ");
