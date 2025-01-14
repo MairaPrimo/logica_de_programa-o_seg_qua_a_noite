34918a26880fdd2eef01973e006cddf201917312
MAIRA PRIMO DE SOUZA <maira.psouza@senacsp.edu.br>
2025-01-13 22:12:34 -0300

    ex04

---
+++ ex04/ex04/Program.cs
@@ -0,0 +1,33 @@
+﻿// tipos de dados
+
+string nome = "Maira";
+Console.WriteLine("o nome do usuario é" + nome );
+
+int idade = 22;
+// Console.WriteLine(" A idade do usuario é: " + idade + "anos" );
+Console.WriteLine ( $" A idade do usuario é: {idade} anos");
+
+float carteira = 2.63f;
+
+ char classificacao = 'A' ;
+Console.WriteLine($" O cliente é do tipo {classificacao}");
+
+bool dinheiroNaconta = true;
+
+
+Double limiteCredito = 2000.0;
+Console.WriteLine("Seu limite de crédito é: " + limiteCredito);
+
+String sobrenome = "Primo";
+
+String nomeCompleto = nome + sobrenome; 
+Console.WriteLine( nomeCompleto);
+
+double valorConta = 53.73;
+Console.WriteLine($"O valor na conta é: {valorConta} ");
+
+double limiteTotal = valorConta + limiteCredito;
+Console.WriteLine($" O saldo + limite é {limiteTotal}");
+
+
+
