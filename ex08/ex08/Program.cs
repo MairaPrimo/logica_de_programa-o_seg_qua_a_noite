016c5fee2157f26a68cabfdf3896cc24343bdaa7
MAIRA PRIMO DE SOUZA <maira.psouza@senacsp.edu.br>
2025-01-15 22:17:52 -0300

    atualização

---
+++ ex08/ex08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Exercício 8: Crie um programa que solicite ao usuário um número e imprima todos os números de 1 até o número informado usando um laço `while`.
+
+Console.WriteLine("Digite um numero");
+
+int numero = int.Parse(Console.ReadLine());
+int contador = 10;
+
+
+// usar do -white para imprimir numeros de 10 até o numero informado ( decrementando de 2 em 2) 
+do
+{
+    Console.WriteLine(contador);
+    contador -= 5;
+}
+
+while (contador >= numero);
+{
+
+}
--- logica-programacao/ex02/obj/Debug/net8.0/ex02.AssemblyInfo.cs
+++ logica-programacao/ex02/obj/Debug/net8.0/ex02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03af102e4abc0f9fcc055d69b5507e0b7683fec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
